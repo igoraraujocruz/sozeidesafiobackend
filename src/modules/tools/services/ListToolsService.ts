8143def159c87bb36850d31f499fa369911f65d0
Igor <igoraraujocruzz@gmail.com>
2021-03-30 21:14:13 -0300

    new rule for the method get

--- src/modules/tools/services/ListToolsService.ts
+++ src/modules/tools/services/ListToolsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import IToolsRepository from '@modules/tools/repositories/IToolsRepository';
 import Tools from '@modules/tools/infra/typeorm/entities/Tools';
+import AppError from '@shared/errors/AppError';
 
 
 interface IRequestDTO {
@@ -16,6 +17,9 @@ class ListToolsService {
 
     public async execute({tag}:IRequestDTO): Promise<Tools[]> {
         const tool = await this.toolsRepository.findAllTools(tag);
+        if (tool.length == 0) {
+            throw new AppError(`I didn't find any tools with that tag`);
+        }
         return tool;
     }
 }
